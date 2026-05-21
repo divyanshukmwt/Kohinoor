56b242470ebc54295bd98e42405ac9cd2635f7d0
sunidhi5678 <chouhansunidhi085@gmail.com>
2026-05-21 21:33:05 +0530

    feat: product insert working in PostgreSQL and Pinecone

--- Backend/database/insert_product.py
+++ Backend/database/insert_product.py
@@ -1,24 +1,64 @@
-from connection import engine
-
+import os
+from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker
-
+from connection import engine
 from models import Product
+from pinecone import Pinecone
 
+load_dotenv()
+
+# PostgreSQL session
 Session = sessionmaker(bind=engine)
-
 session = Session()
 
-product1 = Product(
-    name="Gold Ring",
-    category="Ring",
-    price=5000,
-    stock=10,
-    image_url="uploads/ring.jpg",
-    description="Beautiful Gold Ring"
+# Pinecone connect
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+index = pc.Index(os.getenv("PINECONE_INDEX"))
+
+# Sample product
+product = Product(
+    id="prd_102",
+    slug="gold-diamond-necklace-2",
+    name="Gold Diamond Necklace",
+    description="Elegant 18K gold diamond necklace for weddings and anniversaries.",
+    price=52400,
+    stock=5,
+    images=["https://ik.imagekit.io/kohinoor/necklace.jpg"],
+    category="necklace",
+    collection="bridal",
+    materials=["gold", "diamond"],
+    gemstones=["diamond"],
+    gender=["women"],
+    occasions=["wedding", "anniversary"],
+    style_tags=["elegant", "luxury"],
+    wear_type="neck",
+    embedding_text="Elegant 18K gold diamond necklace designed for wedding styling and anniversary gifting with timeless luxury aesthetics.",
+    related_products=[],
+    complete_look=[],
+    featured=True
 )
 
-session.add(product1)
-
+# PostgreSQL mein insert
+session.add(product)
 session.commit()
+print("Product inserted in PostgreSQL!")
 
-print("Product Inserted Successfully")
+# Pinecone mein insert
+index.upsert(
+    vectors=[
+        {
+            "id": "prd_101",
+            "values": [0.1] * 1024,
+            "metadata": {
+                "category": "necklace",
+                "materials": ["gold", "diamond"],
+                "occasions": ["wedding", "anniversary"],
+                "styleTags": ["elegant", "luxury"],
+                "wearType": "neck",
+                "price": 52400
+            }
+        }
+    ]
+)
+print("Product inserted in Pinecone!")
+print("All Done!")
